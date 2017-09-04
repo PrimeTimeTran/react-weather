98209d93990ff5b6d939eb3b8cf4532d5fdbac33
Loi Tran <lvt15@my.fsu.edu>
2017-09-04 16:59:27 +0700

    Completed basic functionality with ajax request, displaying, refactor of WeatherList

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,10 +1,16 @@
+import axios from 'axios';
+
 const API_KEY = 'd2f75dfb3921840411528b894f9dd863';
-const ROOT_URL = `http://API.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
 export function fetchWeather(searchTerm) {
   const url = `${ROOT_URL}&q=${searchTerm},us`;
+  const request = axios.get(url);
+
   return {
-    type: FETCH_WEATHER
+    type: FETCH_WEATHER,
+    payload: request
   };
 }
--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,16 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { Component } from 'react';
 
 import SearchBar from '../containers/search_bar';
+import WeatherList from '../containers/weather_list';
 
 export default class App extends Component {
   render() {
     return (
-      <SearchBar />
+      <div>
+        <SearchBar />
+        <WeatherList />
+      </div>
     );
   }
 }

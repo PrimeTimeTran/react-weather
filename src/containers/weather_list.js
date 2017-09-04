5a0e87b50dc4aa9edefaa6d08892ecc3eadb0d93
Loi Tran <lvt15@my.fsu.edu>
2017-09-04 18:30:03 +0700

    Cleaned up code for styling

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Chart from '../components/chart';
+import GoogleMap from '../components/google_map';
 
 class WeatherList extends Component {
   renderWeather(cityData) {
@@ -8,10 +9,11 @@ class WeatherList extends Component {
     const temps = _.map(cityData.list.map(weather => weather.main.temp), (temp) => temp * (9/5) -459.67 ) ;
     const pressures = cityData.list.map(weather => weather.main.pressure);
     const humidities = cityData.list.map(weather => weather.main.humidity);
+    const { lon, lat } = cityData.city.coord;
 
     return (
       <tr key={name}>
-        <td>{name}</td>
+        <td><GoogleMap lon={lon} lat={lat} /></td>
         <td><Chart data={temps} color="orange" units="F" /></td>
         <td><Chart data={pressures} color="blue" units="hPa" /></td>
         <td><Chart data={humidities} color="green" units="%" /></td>
